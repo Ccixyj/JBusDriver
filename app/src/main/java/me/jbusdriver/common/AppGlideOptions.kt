c3e8753a991b12dd122b1e5a9715a4729e67879d
徐宇健 <751762476@qq.com>
2017-11-01 10:29:16 +0800

    upgrade glide 4.3.0

--- app/src/main/java/me/jbusdriver/common/AppGlideOptions.kt
+++ app/src/main/java/me/jbusdriver/common/AppGlideOptions.kt
@@ -7,19 +7,16 @@ import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
 import com.bumptech.glide.load.model.GlideUrl
-import com.bumptech.glide.load.model.Headers
 import com.bumptech.glide.module.AppGlideModule
 import me.jbusdriver.http.NetClient
 import java.io.InputStream
-import java.net.URL
 
 /**
  * Created by Administrator on 2016/7/22 0022.
  */
 @GlideModule
-open  class AppGlideOptions : AppGlideModule() {
+open class AppGlideOptions : AppGlideModule() {
     override fun applyOptions(context: Context, builder: GlideBuilder) {
-
     }
 
     override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
@@ -30,11 +27,7 @@ open  class AppGlideOptions : AppGlideModule() {
 
 }
 
-class GlideNoHost : GlideUrl {
-    constructor(url: URL) : super(url)
-    constructor(url: String) : super(url)
-    constructor(url: URL, headers: Headers?) : super(url, headers)
-    constructor(url: String, headers: Headers?) : super(url, headers)
+class GlideNoHost(url: String) : GlideUrl(url) {
 
     override fun getCacheKey() = toStringUrl().urlPath
 }
