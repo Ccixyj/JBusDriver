60c19b658ae39ab4279403168544b6a32783254c
来者可是诸葛bloo <751762476@qq.com>
2018-04-16 00:58:11 +0800

    add like movie/actress

---
+++ app/src/main/java/me/jbusdriver/ui/fragment/RecommendListFragment.kt
@@ -0,0 +1,97 @@
+package me.jbusdriver.ui.fragment
+
+import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.bumptech.glide.Glide
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.BaseViewHolder
+import com.chad.library.adapter.base.loadmore.LoadMoreView
+import jbusdriver.me.jbusdriver.R
+import kotlinx.android.synthetic.main.layout_recycle.*
+import kotlinx.android.synthetic.main.layout_swipe_recycle.*
+import me.jbusdriver.common.AppBaseRecycleFragment
+import me.jbusdriver.common.KLog
+import me.jbusdriver.http.JAVBusService.Companion.defaultFastUrl
+import me.jbusdriver.http.JAVBusService.Companion.defaultImageUrlHosts
+import me.jbusdriver.mvp.HotRecommendContract
+import me.jbusdriver.mvp.bean.ActressInfo
+import me.jbusdriver.mvp.bean.RecommendRespBean
+import me.jbusdriver.mvp.presenter.HotRecommendPresenterImpl
+import me.jbusdriver.ui.activity.MovieListActivity
+import me.jbusdriver.ui.activity.SearchResultActivity
+
+/**
+ * Created by Administrator on 2017/7/30.
+ */
+class RecommendListFragment : AppBaseRecycleFragment<HotRecommendContract.HotRecommendPresenter, HotRecommendContract.HotRecommendView, RecommendRespBean>(), HotRecommendContract.HotRecommendView {
+
+
+    override fun createPresenter() = HotRecommendPresenterImpl()
+
+    override val layoutId: Int = R.layout.layout_swipe_recycle
+    override val swipeView: SwipeRefreshLayout?  by lazy { sr_refresh }
+    override val recycleView: RecyclerView by lazy { rv_recycle }
+    override val layoutManager: RecyclerView.LayoutManager  by lazy { LinearLayoutManager(viewContext) }
+
+    override val adapter = object : BaseQuickAdapter<RecommendRespBean, BaseViewHolder>(R.layout.layout_recommend_item) {
+
+        override fun convert(helper: BaseViewHolder, item: RecommendRespBean) {
+            val image = defaultImageUrlHosts[if (item.key.img.endsWith("xyz")) "xyz" else "default"] + item.key.img
+
+            Glide.with(viewContext).load(image).into(helper.getView(R.id.iv_recommend_img))
+            helper.setText(R.id.tv_recommend_title, item.key.name)
+                    .setText(R.id.tv_reason, if (item.reason?.isNotBlank() == true) "推荐理由：${item.reason}" else "")
+                    .setText(R.id.tv_recommend_score, item.score.toString())
+
+
+        }
+
+    }
+
+    override fun initWidget(rootView: View) {
+        super.initWidget(rootView)
+        adapter.setOnLoadMoreListener({
+            KLog.d("onLoadMore")
+            mBasePresenter?.onLoadMore()
+
+        }, recycleView)
+        adapter.setLoadMoreView(object : LoadMoreView() {
+            override fun getLayoutId(): Int = R.layout.layout_load_reset
+
+            override fun getLoadingViewId(): Int = R.id.tv_end
+
+            override fun getLoadEndViewId(): Int = R.id.tv_end
+
+            override fun getLoadFailViewId(): Int = R.id.tv_end
+        })
+        adapter.setOnItemClickListener { _, view, position ->
+            adapter.getItem(position)?.let {
+                KLog.d(it.key)
+                val image = defaultImageUrlHosts[if (it.key.img.endsWith("xyz")) "xyz" else "default"] + it.key.img
+                if (it.key.url.contains("/star/", false)) {
+                    MovieListActivity.start(viewContext, ActressInfo(it.key.name, image, defaultFastUrl + it.key.url))
+                } else {
+                    SearchResultActivity.start(this.viewContext,it.key.name.split(" ").component1())
+                  //  MovieDetailActivity.start(this.viewContext, Movie(), false)
+                }
+            }
+        }
+    }
+
+    override fun showContents(data: List<*>) {
+        adapter.data.clear()
+        super.showContents(data)
+    }
+
+    override fun loadMoreEnd(clickable: Boolean) {
+        super.loadMoreEnd(true)
+    }
+
+    companion object {
+        fun newInstance() = RecommendListFragment()
+
+    }
+
+}
