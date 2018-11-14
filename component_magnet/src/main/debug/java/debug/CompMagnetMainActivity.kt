2ddca75eec230b33129da30621d407fb4a1ffb3d
xuyujian <15152278073@139.com>
2018-11-14 17:06:45 +0800

    [log] set logger instance only [magnet] add component caller test

--- component_magnet/src/main/debug/java/debug/CompMagnetMainActivity.kt
+++ component_magnet/src/main/debug/java/debug/CompMagnetMainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.billy.cc.core.component.CC
 import kotlinx.android.synthetic.main.comp_magnet_activity_main.*
+import me.jbusdriver.base.KLog
 import me.jbusdriver.base.common.C
 import me.jbusdriver.component.magnet.R
 
 class CompMagnetMainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +19,35 @@ class CompMagnetMainActivity : AppCompatActivity() {
                     .setActionName("show")
                     .addParam("keyword", et_keyword.text.toString())
                     .build().call()
+        }
+
+        //"allKeys" , "config.save" , "config.getKeys"
+        comp_magnet_tv_get_all.setOnClickListener {
+            CC.obtainBuilder(C.Components.Manget)
+                    .setActionName("allKeys")
+                    .build().call()
+        }
+
+        comp_magnet_tv_config_get.setOnClickListener {
+            CC.obtainBuilder(C.Components.Manget)
+                    .setActionName("config.getKeys")
+                    .build().call()
+        }
+
+        comp_magnet_tv_config_save.setOnClickListener {
+            val res = CC.obtainBuilder(C.Components.Manget)
+                    .setActionName("allKeys")
+                    .build().call()
+            if (res.isSuccess) {
+                val allKeys = res.getDataItem<List<String>>("keys")
+                CC.obtainBuilder(C.Components.Manget)
+                        .setActionName("config.save")
+                        .addParam("keys", allKeys.shuffled().take((Math.random() * allKeys.size).toInt().coerceIn(1..allKeys.size)))
+                        .build().call()
+            } else {
+                KLog.w("error get all key : ${res.errorMessage}")
+            }
+
         }
     }
 }
---
+++ component_magnet/src/main/debug/java/debug/DebugApp.kt
@@ -0,0 +1,8 @@
+package debug
+
+import me.jbusdriver.base.cc.debug.BaseApp
+
+class DebugApp : BaseApp() {
+
+    override fun getTag() = "comp_magnet"
+}
