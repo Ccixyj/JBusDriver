4b19f126f12291fdc87bf511481c10f7780e61e2
来者可是诸葛bloo <751762476@qq.com>
2019-03-20 20:02:03 +0800

    [update] switch branch when mode change

--- app/src/main/java/me/jbusdriver/http/GitHub.kt
+++ app/src/main/java/me/jbusdriver/http/GitHub.kt
@@ -1,16 +1,18 @@
 package me.jbusdriver.http
 
 import io.reactivex.Flowable
+import me.jbusdriver.BuildConfig
 import me.jbusdriver.base.http.NetClient
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 
 /**
  * Created by Administrator on 2017/4/15 0015.
  */
 interface GitHub {
-    @GET("https://raw.githubusercontent.com/Ccixyj/JBusDriver/master/api/announce.json")
-    fun announce(): Flowable<String>
+    @GET("https://raw.githubusercontent.com/Ccixyj/JBusDriver/{branch}/api/announce.json")
+    fun announce(@Path("branch") branch: String = if (BuildConfig.DEBUG) "dev" else "master"): Flowable<String>
 
 //
 //    @GET("https://raw.githubusercontent.com/Ccixyj/JBusDriver/plugin/api/announce.json")
