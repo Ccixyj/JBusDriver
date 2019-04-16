4359fa5f765e51fefaa6351c777d3babe0008d8f
xuyujian <15152278073@139.com>
2019-04-16 14:41:26 +0800

    [plugin_magnet] fix btdigg

--- plugins/plugin_magnet/src/main/java/me/jbusdriver/plugin/magnet/loaders/BtdiggsMagnetLoaderImpl.kt
+++ plugins/plugin_magnet/src/main/java/me/jbusdriver/plugin/magnet/loaders/BtdiggsMagnetLoaderImpl.kt
@@ -1,17 +1,14 @@
 package me.jbusdriver.plugin.magnet.loaders
 
 import android.util.Log
-
 import me.jbusdriver.plugin.magnet.IMagnetLoader
 import me.jbusdriver.plugin.magnet.IMagnetLoader.Companion.safeJsoupGet
-import me.jbusdriver.plugin.magnet.initHeaders
 import me.jbusdriver.plugin.magnet.loaders.EncodeHelper.encodeBase64
 import org.json.JSONObject
-import org.jsoup.Jsoup
 
 class BtdiggsMagnetLoaderImpl : IMagnetLoader {
     //  key -> page
-    private val search = "https://www.btdigg.xyz/search/%s/%s/1/0.html"
+    private val search = "http://btdiggs.org/search/%s/%s/1/0.html"
 
     override var hasNexPage: Boolean = false
     val TAG = "MagnetLoader:Btdiggs"
@@ -20,7 +17,7 @@ class BtdiggsMagnetLoaderImpl : IMagnetLoader {
         val formatUrl = search.format(encodeBase64(key), page)
         Log.w(TAG, "load url :$formatUrl")
         return try {
-            val doc = IMagnetLoader.safeJsoupGet(formatUrl) ?: return emptyList()
+            val doc = safeJsoupGet(formatUrl) ?: return emptyList()
             Log.i(TAG, "load doc :${doc.title()}")
             hasNexPage = doc.select(".page-split :last-child[title]").size > 0
             doc.select(".list dl").map {
@@ -29,11 +26,11 @@ class BtdiggsMagnetLoaderImpl : IMagnetLoader {
                 val url = href.attr("href")
 
                 val realUrl = when {
-                    url.startsWith("www.") -> "https://$url"
-                    url.startsWith("/magnet") -> {
-                        IMagnetLoader.MagnetFormatPrefix + url.removePrefix("/magnet/").removeSuffix(".html")
-                    }
-                    else -> "https://www.btdigg.xyz$url"
+                    url.startsWith("www.")  || url.startsWith("//")-> "http://${url.removePrefix("//")}"
+                    url.startsWith("/magnet") -> IMagnetLoader.MagnetFormatPrefix + url.removePrefix("/magnet/").removeSuffix(
+                        suffix = ".html"
+                    )
+                    else -> "http://btdiggs.org$url"
                 }
 
                 val labels = it.select(".attr span")
@@ -56,7 +53,7 @@ class BtdiggsMagnetLoaderImpl : IMagnetLoader {
 
 
     override fun fetchMagnetLink(url: String): String {
-        return (IMagnetLoader.MagnetFormatPrefix +     safeJsoupGet(url)?.select(".content .infohash")?.text()?.trim().orEmpty())
+        return (IMagnetLoader.MagnetFormatPrefix + safeJsoupGet(url)?.select(".content .infohash")?.text()?.trim().orEmpty())
     }
 }
 
