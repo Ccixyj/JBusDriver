1dfb5843c3c09a01887bffc8afafd3734b70597b
xuyujian <xuyujian@ysten.com>
2022-05-26 15:28:10 +0800

    update plugin v1.1.1

--- plugins/plugin_magnet/src/main/java/me/jbusdriver/plugin/magnet/loaders/MagnetLoaders.kt
+++ plugins/plugin_magnet/src/main/java/me/jbusdriver/plugin/magnet/loaders/MagnetLoaders.kt
@@ -7,19 +7,18 @@ object MagnetLoaders {
      */
     val Loaders: Map<String, IMagnetLoader> by lazy {
         mapOf(
-            "default" to DefaultLoaderImpl(),
-            "超人" to ChaoRenLoaderImpl(),
-            "Btanv" to BtAntMagnetLoaderImpl(),
-            "Kitty" to CNBtkittyMangetLoaderImpl(),
-            "btdigg" to BtdiggsMagnetLoaderImpl(),
-            "zzjd" to ZZJDMagnetLoaderImpl(),
-            "BTbaocai" to BTBCMagnetLoaderImpl(),
-            "BTSO.PW" to BtsoPWMagnetLoaderImpl(),
-            "BTSOW" to BTSOWMagnetLoaderImpl(),
-           /* "BTDB" to BTDBMagnetLoaderImpl(),*/
-            "btcherries" to BTCherryMagnetLoaderImpl(),
-            "TorrentKitty" to TorrentKittyMangetLoaderImpl()
-
+            "default" to DefaultLoaderImpl()
+//            "超人" to ChaoRenLoaderImpl(),
+//            "Btanv" to BtAntMagnetLoaderImpl(),
+//            "Kitty" to CNBtkittyMangetLoaderImpl(),
+//            "btdigg" to BtdiggsMagnetLoaderImpl(),
+//            "zzjd" to ZZJDMagnetLoaderImpl(),
+//            "BTbaocai" to BTBCMagnetLoaderImpl(),
+//            "BTSO.PW" to BtsoPWMagnetLoaderImpl(),
+//            "BTSOW" to BTSOWMagnetLoaderImpl(),
+//            "BTDB" to BTDBMagnetLoaderImpl(),
+//            "btcherries" to BTCherryMagnetLoaderImpl(),
+//            "TorrentKitty" to TorrentKittyMangetLoaderImpl()
         )
     }
 
